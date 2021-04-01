d84edc677fc01bac819677d0cf6a6f9daa26fc4c
zakiyul <zakiyul.py@gmail.com>
2021-04-01 22:01:33 +0800

    setup and configuration

--- app/config.js
+++ app/config.js
@@ -3,4 +3,11 @@ dotenv.config();
 
 module.exports = {
   serviceName: process.env.SERVICE_NAME,
+
+  // KONFIGURASI DATABASE
+  dbHost: process.env.DB_HOST,
+  dbUser: process.env.DB_USER,
+  dbPort: process.env.DB_PORT,
+  dbPass: process.env.DB_PASS,
+  dbName: process.env.DB_NAME,
 };
