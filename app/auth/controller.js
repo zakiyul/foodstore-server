d443d0a206278e99d061b5274b82feea58a3f252
zakiyul <zakiyul.py@gmail.com>
2021-04-23 13:45:40 +0800

    logout route

--- app/auth/controller.js
+++ app/auth/controller.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const User = require("../user/model");
 const config = require("../config");
+const { getToken } = require("../utils/get-token");
 
 async function register(req, res, next) {
   try {
@@ -58,8 +59,41 @@ async function login(req, res, next) {
   })(req, res, next);
 }
 
+function me(req, res, next) {
+  if (!req.user) {
+    return res.json({
+      error: 1,
+      message: `Your're not login or token expired`,
+    });
+  }
+  return res.json(req.user);
+}
+
+async function logout(req, res, next) {
+  let token = getToken(req);
+  let user = await User.findOneAndUpdate(
+    { token: { $in: [token] } },
+    { $pull: { token } },
+    { useFindAndModify: false }
+  );
+
+  if (!user || !token) {
+    return res.json({
+      error: 1,
+      message: "No user found",
+    });
+  }
+
+  return res.json({
+    error: 0,
+    message: "Logout berhasil",
+  });
+}
+
 module.exports = {
   register,
   localStrategy,
   login,
+  me,
+  logout,
 };
--- app/auth/router.js
+++ app/auth/router.js
@@ -10,5 +10,7 @@ passport.use(
 
 router.post("/register", multer().none(), controller.register);
 router.post("/login", multer().none(), controller.login);
+router.get("/me", controller.me);
+router.post("/logout", controller.logout);
 
 module.exports = router;
